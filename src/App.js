fc35765ece035adb5fc8558ee1d6ac31ef777465
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-10-02 17:22:34 +0530

    Flex Property practice.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import { React } from 'react';
 import './App.scss';
 
 const App = () =>
-	<div className="App">
-		Ready to start.
+	<div className="square">
+		<div className="square1">squ1</div>
+		<div className="square2">squ2</div>
+		<div className="square3">squ3</div>
+		<div className="square4">squ4</div>
+		<div className="square5">squ5</div>
 	</div>;
 
 export default App;
